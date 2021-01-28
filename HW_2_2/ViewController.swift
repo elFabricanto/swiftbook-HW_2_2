7b945f5e17d04362bffff78680a088461e087a93
Chupin Daniil <chupindaniil@gmail.com>
2021-01-28 23:42:19 +0700

    Corrections were made according to Alex's comments

--- HW_2_2/ViewController.swift
+++ HW_2_2/ViewController.swift
@@ -9,23 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var clickCounter = 0
+    
+    // MARK: - IB Outlets
     @IBOutlet var lightChanger: UIButton!
     @IBOutlet weak var light: UIStackView!
     
+    // MARK: - Private Properties
+    private var clickCounter = 0
+    
+    // MARK: - Life Cycles Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         turnLightOff()
     }
     
-    func turnLightOff() {
+    //MARK: - IB Actions
+    @IBAction func changeLightColor() {
+        lightChanger.setTitle("NEXT", for: .normal)
+        turnNextLightOn()
+    }
+    
+    //MARK: - Private Methods
+    private func turnLightOff() {
         for lightItem in light.arrangedSubviews {
-            lightItem.layer.cornerRadius = lightItem.bounds.size.width / 2
+            lightItem.layer.cornerRadius = lightItem.frame.width / 2
             lightItem.alpha = 0.3
         }
     }
     
-    func turnNextLightOn() {
+    private func turnNextLightOn() {
         let currentLightItem = clickCounter % (light.arrangedSubviews.count * 2 - 2)
         if currentLightItem < light.arrangedSubviews.count {
             turnLightOff()
@@ -35,11 +47,6 @@ class ViewController: UIViewController {
             light.arrangedSubviews[currentLightItem - light.arrangedSubviews.count + 1].alpha = 1
         }
         clickCounter += 1
-    }
-
-    @IBAction func changeLightColor() {
-        lightChanger.setTitle("NEXT", for: .normal)
-        turnNextLightOn()
     }
     
 }
